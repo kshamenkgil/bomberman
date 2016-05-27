a641b8a722722f0431a5a379b40553cc4ea86193
Kshamenk Gil <kshamenk.n.gil@gmail.com>
2016-05-27 16:51:19 -0300

    JugadorAgarraVida

--- Bomberman/src/bomberman/Jugador.java
+++ Bomberman/src/bomberman/Jugador.java
@@ -1,18 +1,45 @@
 package bomberman;
 
 public class Jugador extends Personaje {
-	public int id; //aleatorio segun ingreso al servidor	
-	public int vidas; 
-	public float potenciaBomba;
+	private int id; //aleatorio segun ingreso al servidor	
+	private int vidas; 
+	private float potenciaBomba;
 	
 	
 	public Jugador() {
-		
-	}
-
+		this.vidas = 4;
+		this.potenciaBomba = 10.0f;
+	}	
+	
 	@Override
 	public void atacar() {
 		// TODO Auto-generated method stub
 		
 	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public void setVidas(int vidas) {
+		this.vidas = vidas;
+	}
+	
+	public int getVidas() {
+		return vidas;
+	}
+	
+	public void setPotenciaBomba(float potenciaBomba) {
+		this.potenciaBomba = potenciaBomba;
+	}
+	
+	public float getPotenciaBomba() {
+		return potenciaBomba;
+	}
+	
+	
 }
--- Bomberman/src/bomberman/VidaExtra.java
+++ Bomberman/src/bomberman/VidaExtra.java
@@ -5,7 +5,7 @@ public class VidaExtra extends Potenciador {
 	@Override
 	public void potenciar(Jugador jugador) {
 		// TODO Auto-generated method stub
-
+		jugador.setVidas(jugador.getVidas()+1);
 	}
 
 }
