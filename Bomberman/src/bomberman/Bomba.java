28f689ed576c2e9e0a1382a68df976a7b02751f5
juliandel <julian_del@hotmail.com>
2016-06-03 18:58:22 -0300

    Update Bomba.java

--- Bomberman/src/bomberman/Bomba.java
+++ Bomberman/src/bomberman/Bomba.java
@@ -4,9 +4,11 @@ public class Bomba {
 	private Sprite bombaSprite;
 	private float potencia;
 	private float tiempoExplosion;
+	private Punto2D ubicacion; //no deberia tambien tener atributo ubicacion?
+	
 		
-	public Bomba(float potencia, float tiempoExplosion) {
-		// TODO Auto-generated constructor stub
+	public Bomba(float potencia, float tiempoExplosion,Punto2D ubic) {
+		ubicacion = new Punto2D(ubic.getX(),ubic.getY());
 	}
 	
 	public void explotar(float tiempoExplocion){
