be5ca04e084e60614ac89905c3af0249ee51f733
Rama <ramaoricchio@gmail.com>
2016-06-29 02:29:18 -0300

    Agregadas 2 clases para test

---
+++ Bomberman/src/tests/FuncionamientoBomba.java
@@ -0,0 +1,43 @@
+package tests;
+
+public class FuncionamientoBomba {
+	
+	/*
+@Test
+public void queSeDestruyaUnaPared() {
+
+	/*Regla: Cuando la explosión de una 
+*bomba alcanza a una pared “destruible”
+*la misma debe desaparecer del mapa
+*/
+/*
+Pared pared = new Pared(0,0);
+Mapa mapa = new Mapa();
+Bomba bomba = new Bomba();
+
+punto = pared.getPosicion();
+	bomba.explotar(0,1);
+	
+Assert.assertEquals(false,mapa.hayPared(punto));
+}
+	 * 
+	 */
+	
+/*
+ * @Test
+public void queUnJugadorPierdaUnaVida(){
+		
+	/*Regla: Los personajes tienen una cantidad
+	* determinada de vidas, cuando este muere
+	* se le descuenta una vida hasta llegar a 0
+	*/
+/*
+	Personaje personaje = new Personaje();
+	Assert.assertEquals(4, p1.getVidas());
+		
+	personaje.quitarUnaVida();
+	Assert.assertEquals(3, personaje.getVidas());				
+	}
+ */
+
+}
---
+++ Bomberman/src/tests/Movimientos.java
@@ -0,0 +1,72 @@
+package tests;
+
+public class Movimientos {
+/*-------------------------PERSONAJE SE MUEVA---------------
+ @Test
+Public void queElPersonajeSeMueva(){
+/* 0 izquierda 
+1 derecha
+2 arriba 
+3 abajo
+*/
+/*
+Personaje personaje = new Personaje();
+personaje.ubicar(5,5);
+Punto p1 = new Punto(6,5);
+Assert.assertEquals(p1,personaje.mover(1));//derecha
+personaje.ubicar(5,5);
+P1 = new Punto(5,6);
+Assert..assertEquals(p1,personaje.mover(3));//abajo
+P1 = new Punto(4,5);
+personaje.ubicar(5,5);
+Assert..assertEquals(p1,personaje.mover(0));//izquierda
+personaje.ubicar(5,5);
+P1 = new Punto(5,4);
+Assert..assertEquals(p1,personaje.mover(2));//arriba
+}
+ */
+	
+/*-------------------------PERSONAJE NO ATRAVIESE UNA PARED---------------	
+  @Test 
+	public  void queUnPersonajeNoAtravieseUnaPared() {
+	            
+	// Regla: Un personaje no puede atravesar una pared
+	            
+	Personaje personaje = new personaje();
+	            personaje.ubicar(0,0);
+	            Pared pared = new Pared(0,1);
+	            Punto2D punto = new Punto2D(0,0);
+	            punto = personaje.getPosicion();
+	            
+	/* Cuando el personaje se quiere mover hacía la posición
+	 *donde se encuentra una pared, el método mover
+	 * debería retornar el punto correspondiente a la posición
+	 * previa del personaje
+*/
+/*
+	Assert.assertEquals(punto,personaje.mover(1));
+	}
+*/
+	
+	
+/*
+ * @Test
+public void queNoSeAtravieseUnaBomba() {
+
+	/*Regla: Un personaje puede atravesar
+	* una bomba, la misma bloquea el paso 
+	* al igual que lo hace una pared
+	*/
+/*
+	Personaje personaje = new personaje();
+            personaje.ubicar(0,0);
+       	Bomba bomba = new Bomba(0,1);
+            Punto2D punto = new Punto2D;
+            
+punto = personaje.getPosicion();
+
+Assert.assertEquals(punto,personaje.mover(1));
+}
+ */
+	
+}
