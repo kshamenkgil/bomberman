5f64d9598f05c82af631742b24e83d33edde0ee1
Rama <ramaoricchio@gmail.com>
2016-07-02 06:19:39 -0300

    cosas de juli

--- Bomberman/src/bomberman/pantallaRanking.java
+++ Bomberman/src/bomberman/pantallaRanking.java
@@ -1,16 +1,22 @@
 package bomberman;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.EventQueue;
+import java.awt.TextArea;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 import javax.swing.border.EmptyBorder;
+import javax.swing.border.LineBorder;
 
 import database.Conector;
 
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
@@ -27,36 +33,35 @@ public class pantallaRanking extends JFrame {
 	 * Create the frame.
 	 */
 	public pantallaRanking() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setResizable(false);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setBorder(new EmptyBorder(0, 0, 0, 0));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
-		
-		// El comboBox del ranking debe traer a todos los usuarios de la base de datos ordenados de manera descendente por puntuacion
-		JComboBox comboRanking = new JComboBox();
-		comboRanking.setBounds(132, 101, 166, 22);
-		contentPane.add(comboRanking);
+
+		TextArea textArea = new TextArea();
+		textArea.setBounds(48, 67, 320, 121);
 		con =  new Conector();
 		con.connect();
-		listado = con.ranking();
-		for (String l : listado){
-			comboRanking.addItem(l);
-		}
-		JLabel lblRanking = new JLabel("Ranking de usuarios:");
-		lblRanking.setBounds(132, 76, 166, 14);
+		listado = con.ranking(); 
+		 for (int i = 4; i > -1;i--){
+			 textArea.setText(listado.get(i)+"\n"+textArea.getText());   
+		  }
+		 contentPane.add(textArea);
+
+		JLabel lblRanking = new JLabel("Ranking");
+		lblRanking.setBounds(180, 34, 56, 16);
 		contentPane.add(lblRanking);
-		
+
 		JButton btnVolver = new JButton("Volver");
 		btnVolver.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				
-				pantallaRanking.this.dispose();
+			public void actionPerformed(ActionEvent arg0) {
+				dispose();
 			}
 		});
-		btnVolver.setBounds(168, 201, 91, 23);
+		btnVolver.setBounds(157, 194, 97, 25);
 		contentPane.add(btnVolver);
 	}
 }
